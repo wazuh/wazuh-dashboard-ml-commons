ce7a7ab1691e1abf86555f99cd65f47d853b7c61
Guido Modarelli <guidomodarelli25@gmail.com>
2025-09-02 22:11:51 +0000

    remove export for unused type
    streamline type exports to reduce unused imports and improve code maintainability

--- public/dashboard-assistant/modules/installation-manager/domain/types/index.ts
+++ public/dashboard-assistant/modules/installation-manager/domain/types/index.ts
@@ -1,6 +1,5 @@
 export * from './install-ai-dashboard-assistant-dto';
 export * from './step-state';
-export * from './installation-progress';
 export * from './installation-error';
 export * from './installation-result';
 export * from './requests';
